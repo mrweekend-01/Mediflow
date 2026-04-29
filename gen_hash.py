ad1b4bcabc858d20fcae1048424a9106bead769e
mrweekend-01 <alvasebas15@gmail.com>
2026-04-29 10:37:07 -0500

    fix: node 22 dockerfile frontend

---
+++ gen_hash.py
@@ -0,0 +1,7 @@
+﻿import bcrypt
+h1 = bcrypt.hashpw(b'comercial123', bcrypt.gensalt(12)).decode()
+h2 = bcrypt.hashpw(b'marketing123', bcrypt.gensalt(12)).decode()
+sql = "UPDATE usuarios SET password_hash = '" + h1 + "' WHERE email = 'comercial@csjd.com';"
+sql += "UPDATE usuarios SET password_hash = '" + h2 + "' WHERE email = 'marketing@csjd.com';"
+open('/tmp/u.sql', 'w').write(sql)
+print('OK:', h1)
